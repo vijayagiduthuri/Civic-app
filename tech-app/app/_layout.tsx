d9e11f3e9759ad73e55812945446d8391bd1f06e
RevanthSrinivas29 <mallarevanthsrinivas@gmail.com>
2025-09-21 21:11:04 +0530

    commit before prebuild

--- tech-app/app/_layout.tsx
+++ tech-app/app/_layout.tsx
@@ -4,8 +4,10 @@ import './global.css';
 export default function RootLayout() {
   return (
     <Stack>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="issueDetails/[id]" options={{ title: "Issue Details" }} />
+      <Stack.Screen name="issueDetails/[id]" options={{ title: "Issue Details" ,}} />
+      <Stack.Screen name="(map)/navigator" options={{ headerShown: false }} />
     </Stack>
   );
 }
